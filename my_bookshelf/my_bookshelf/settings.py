00c6eeeb387f94846aab181ced0c32e99331c8f0
vhadzhiev <vphadzhiev@gmail.com>
2022-03-29 10:06:35 +0300

    Created auth and web apps

--- my_bookshelf/my_bookshelf/settings.py
+++ my_bookshelf/my_bookshelf/settings.py
@@ -30,14 +30,24 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+)
+
+THIRD_PARTY_APPS = ()
+
+PROJECT_APPS = (
+    'my_bookshelf.auth',
+    'my_bookshelf.web',
+)
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
