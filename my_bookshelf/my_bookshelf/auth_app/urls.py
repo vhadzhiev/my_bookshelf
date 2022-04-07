80858580ddb4caf6cec2e4dcbb3b09386cdb5838
vhadzhiev <vphadzhiev@gmail.com>
2022-04-07 20:12:57 +0300

    Altered templates

--- my_bookshelf/my_bookshelf/auth_app/urls.py
+++ my_bookshelf/my_bookshelf/auth_app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from django.views.generic import TemplateView
 
 from my_bookshelf.auth_app.views import UserRegistrationView, UserLoginView, UserLogoutView, ChangePasswordView, \
@@ -8,9 +8,11 @@ urlpatterns = (
     path('register/', UserRegistrationView.as_view(), name='register user'),
     path('login/', UserLoginView.as_view(), name='login user'),
     path('logout/', UserLogoutView.as_view(), name='logout user'),
+
     path('password_change/', ChangePasswordView.as_view(), name='password change'),
     path('password_change_done/', TemplateView.as_view(template_name='auth/password_change_done.html'),
          name='password change done'),
+
     path('password_reset/', ResetPasswordView.as_view(), name='password reset'),
     path('password_reset_done/', ResetPasswordDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', ResetPasswordConfirmView.as_view(), name='password_reset_confirm'),
