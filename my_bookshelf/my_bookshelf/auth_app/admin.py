119e71c5559cc090d1c3de361dd7d33982ebd283
vhadzhiev <vphadzhiev@gmail.com>
2022-04-13 22:00:32 +0300

    Refactored admin.py

--- my_bookshelf/my_bookshelf/auth_app/admin.py
+++ my_bookshelf/my_bookshelf/auth_app/admin.py
@@ -3,15 +3,14 @@ from django.contrib import admin
 from my_bookshelf.auth_app.models import Profile, MyBookshelfUser
 
 
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user_id', 'first_name', 'last_name', 'user')
-    ordering = ('user_id', 'first_name', 'last_name', 'user')
-    list_filter = ('user_id', 'first_name', 'last_name', 'user')
-
-
 @admin.register(MyBookshelfUser)
 class MyBookshelfUserAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'date_joined', 'last_login', 'is_superuser', 'is_staff', 'is_active')
     ordering = ('id', 'email', 'date_joined', 'last_login', 'is_superuser', 'is_staff', 'is_active')
-    list_filter = ('id', 'email', 'date_joined', 'last_login', 'is_superuser', 'is_staff', 'is_active')
+    list_filter = ('id', 'date_joined', 'last_login', 'is_superuser', 'is_staff', 'is_active')
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'first_name', 'last_name', 'user')
+    ordering = ('user_id', 'first_name', 'last_name', 'user')
