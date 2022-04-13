7e0f431d1f75d30c883654fd657450548e589f5d
vhadzhiev <vphadzhiev@gmail.com>
2022-04-13 14:19:08 +0300

    registered book and bookshelf admins

--- my_bookshelf/my_bookshelf/web_app/admin.py
+++ my_bookshelf/my_bookshelf/web_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from my_bookshelf.web_app.models import Book, Bookshelf
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'isbn', 'genre', 'date_added', 'user')
+    ordering = ('title', 'author', 'date_added', 'user')
+    list_filter = ('genre', 'user')
+
+
+@admin.register(Bookshelf)
+class BookshelfAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_added', 'user')
+    ordering = ('title', 'date_added', 'user')
+    list_filter = ('user',)
